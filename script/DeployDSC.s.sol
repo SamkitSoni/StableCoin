0c8bdde6e36b14032fbae6c2a023dcd5c87f8b30
SamkitSoni <samkitsoni09@gmail.com>
2025-04-03 06:08:08 +0000

    Added deployment script and network config

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {DeployDSC} from "../script/DeployDSC.s.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDSC is Script {
+    address[] public tokenAdresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DSCEngine, DecentralizedStableCoin) {
+        HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+
+        tokenAdresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine dscEngine = new DSCEngine(tokenAdresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(dscEngine));
+        vm.stopBroadcast();
+        return (dscEngine, dsc);
+    }
+}
